b7b40a616b859d22e3cec1b0f5d8d5d6e5bdf27f
oatmealb <oatmealb@example.com>
2025-01-24 13:52:50 +0100

    .

--- raw_nike.py
+++ raw_nike.py
@@ -1,18 +1,21 @@
 from seleniumbase import SB
-
+from time import sleep
+import random
+def _sleep():
+  sleep(random.uniform(0.2,1.5))
 with SB(uc=True, test=True, locale_code="en", ad_block=True) as sb:
     url = "https://www.nike.com/"
     sb.activate_cdp_mode(url)
     sb.sleep(2.5)
     t=sb.find_element('body')
     # t=sb.find_element('.property-section')
-    print(f'.prop: {t}')
-    print(f'.prop.text: {t.text}')
-    t=sb.remove_attributes('script')
+    # print(f'.prop: {t}')
+    # print(f'.prop.text: {t.text}')
+    t=sb.remove_elements('script')
     print(f'.prop WOO script: {t}')
-    print(f'.prop WOO script.text: {t.text}')
-    t=sb.remove_attributes('style')
-    t=sb.remove_attributes('.jd-ekskluziva')
+    # print(f'.prop WOO script.text: {t.text}')
+    t=sb.remove_elements('style')
+    t=sb.remove_elements('.jd-ekskluziva')
 
 
     # sb.cdp.gui_click_element('div[data-testid="user-tools-container"]')
