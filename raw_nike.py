468066471de610b9494e8c34e0e3be68b99214db
Blaz Merela <blaz@merela.org>
2025-01-24 12:37:13 +0100

    .

--- raw_nike.py
+++ raw_nike.py
@@ -4,14 +4,43 @@ with SB(uc=True, test=True, locale_code="en", ad_block=True) as sb:
     url = "https://www.nike.com/"
     sb.activate_cdp_mode(url)
     sb.sleep(2.5)
-    sb.cdp.gui_click_element('div[data-testid="user-tools-container"]')
-    sb.sleep(1.5)
-    search = "Nike Air Force 1"
-    sb.cdp.press_keys('input[type="search"]', search)
-    sb.sleep(4)
-    elements = sb.cdp.select_all('ul[data-testid*="products"] figure .details')
-    if elements:
-        print('**** Found results for "%s": ****' % search)
-    for element in elements:
-        print("* " + element.text)
-    sb.sleep(2)
+    # t=sb.get_text('.property-section')
+    t=sb.find_element('.property-section')
+    print(f'.prop: {t}')
+    print(f'.prop.text: {t.text}')
+    t=sb.remove_attributes('script')
+    print(f'.prop WOO script: {t}')
+    print(f'.prop WOO script.text: {t.text}')
+
+
+    # sb.cdp.gui_click_element('div[data-testid="user-tools-container"]')
+    # sb.sleep(1.5)
+    # search = "Nike Air Force 1"
+    # sb.cdp.press_keys('input[type="search"]', search)
+    # sb.sleep(4)
+    # elements = sb.cdp.select_all('ul[data-testid*="products"] figure .details')
+    # if elements:
+    #     print('**** Found results for "%s": ****' % search)
+    # for element in elements:
+    #     print("* " + element.text)
+
+
+# from bs4 import BeautifulSoup
+
+# # Assuming 'html_content' is your HTML string
+# soup = BeautifulSoup(html_content, 'html.parser')
+
+# # Remove script tags
+# for script in soup.find_all('script'):
+#     script.decompose()
+
+# # Remove style tags
+# for style in soup.find_all('style'):
+#     style.decompose()
+
+# # Remove elements with class 'jd-ekskluziva'
+# for ekskluziva in soup.find_all(class_='jd-ekskluziva'):
+#     ekskluziva.decompose()
+
+# # Convert to string
+# result = str(soup)
--- s.py
+++ s.py
@@ -1,12 +1,12 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True, locale_code="en") as sb:
-    url = "https://gitlab.com/users/sign_in"
+    url = "https://www.nepremicnine.net/oglasi-oddaja/juzna-primorska/koper/koper/"
     sb.activate_cdp_mode(url)
-    sb.uc_gui_click_captcha()
-    sb.assert_text("Username", '[for="user_login"]', timeout=3)
-    sb.assert_element('label[for="user_login"]')
-    sb.highlight('button:contains("Sign in")')
-    sb.highlight('h1:contains("GitLab.com")')
-    sb.post_message("SeleniumBase wasn't detected", duration=4)
-    print("Success! Website did not detect SeleniumBase!")
+    sb.sleep(2.5)
+    t=sb.get_text('.property-section')
+$('.property-section')
+    .remove('script')
+    .remove('style')
+    .remove('.jd-ekskluziva')
+    .toString()
